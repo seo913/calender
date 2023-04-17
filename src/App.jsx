8c4a732daa9c2e2cc841a93417131a7ecd48489f
seojaemin <seojaemin@seojaemin-ui-MacBookPro.local>
2023-04-17 22:36:23 +0900

    calender

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Calender from "./components/Calendar";
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-200 flex justify-center items-center">
+      <Calender />
+    </div>
+  );
+}
+
+export default App;
